d642cda900ecd1524e88dba342a1d149aec28615
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-15 09:20:02 -0300

    IoC

---
+++ src/main/java/InversaodeControle/EnviarEmails.java
@@ -0,0 +1,17 @@
+package InversaodeControle;
+
+public class EnviarEmails {
+
+    public EnviarEmails(String tipo, String endereco, String senha){
+
+
+    }
+
+    public static EnviarEmails obterDadosEmail (){
+       return new EnviarEmails("email", "contato@email.com", "senha");
+    }
+
+    public void retornar (String mensagem){
+        System.out.println("Email enviado!");
+    }
+}
